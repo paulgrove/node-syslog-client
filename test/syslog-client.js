63d865443a6e092f980f1ae665ee4ef49c2f101a
Paul Grove <paul.grove@accumuli.com>
2016-08-30 18:34:28 +0100

    More tests for coverage and some smaller bugs

--- test/syslog-client.js
+++ test/syslog-client.js
@@ -9,6 +9,7 @@ var chai = require("chai"),
 	net = require("net"),
 	rl = require("readline"),
 	os = require("os"),
+	Promise = require("bluebird"),
 	queuedSyslogUdpMessages = [],
 	pendingSyslogUdpPromises = [],
 	queuedSyslogTcpMessages = [],
@@ -245,6 +246,247 @@ describe("Syslog Client", function () {
 		})
 		.then(function (msg) {
 			assert.equal(transport_, client.transport_);
+		});
+	});
+	it("should call close event when closed UDP", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogUdpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Udp
+		});
+
+		client.log("Transport close test");
+		client.once("close", function () {
+			assert.equal(client.transport_, undefined);
+			client.once("close", function () {
+				assert.equal(client.transport_, undefined);
+				done();
+			});
+			client.close();
+		});
+
+		awaitSyslogUdpMsg()
+		.then(function (msg) {
+			client.close();
+		})
+	});
+	it("should call close event when closed TCP", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogTcpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		client.log("Transport close test");
+		client.once("close", function () {
+			assert.equal(client.transport_, undefined);
+			client.once("close", function () {
+				assert.equal(client.transport_, undefined);
+				done();
+			});
+			client.close();
+		});
+
+		awaitSyslogTcpMsg()
+		.then(function (msg) {
+			client.close();
+		})
+	});
+	it("should reconnect after connection is closed UDP", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogUdpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Udp
+		});
+
+		client.log("Transport close test");
+		client.once("close", function () {
+			client.log("Restart connection test");
+			awaitSyslogUdpMsg()
+			.then(function (msg) {
+				assert.match(msg, constructSyslogRegex(134, hostname,
+					"Restart connection test"));
+				done();
+			})
+		});
+
+		awaitSyslogUdpMsg()
+		.then(function (msg) {
+			client.close();
+		})
+	});
+	it("should reconnect after connection is closed TCP", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogTcpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		client.log("Transport close test");
+		client.once("close", function () {
+			client.log("Restart connection test");
+			awaitSyslogTcpMsg()
+			.then(function (msg) {
+				assert.match(msg, constructSyslogRegex(134, hostname,
+					"Restart connection test"));
+				done();
+			})
+		});
+
+		awaitSyslogTcpMsg()
+		.then(function (msg) {
+			client.close();
+		})
+	});
+	it("should throw if a string isnt provided to .log()", function () {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogTcpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		(function () {
+			client.log();
+		}).should.throw(Error);
+
+		(function () {
+			client.log({});
+		}).should.throw(Error);
+
+		(function () {
+			client.log([]);
+		}).should.throw(Error);
+		
+		(function () {
+			client.log(undefined);
+		}).should.throw(Error);
+		
+		(function () {
+			client.log(null);
+		}).should.throw(Error);
+	});
+	it("should take a callback as the second argument to .log", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogTcpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		var count = 2,
+			decFn = function () {
+				count--;
+				if (count === 0)
+					done();
+			};
+
+		client.log("anything", decFn);
+		awaitSyslogTcpMsg().then(decFn)
+	});
+	it("should take options as the second argument to .log", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: syslogTcpPort,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		var count = 2,
+			decFn = function () {
+				count--;
+				if (count === 0)
+					done();
+			};
+
+		client.log("anything", {
+			facility: syslogClient.Facility.System,
+			severity: syslogClient.Severity.Notice
+		}, decFn);
+		awaitSyslogTcpMsg().then(function (msg) {
+			assert.match(msg, constructSyslogRegex(29, hostname,
+				"anything"));
+			decFn();
+		});
+	});
+	it("should call on error on connection error Tcp when invalid port", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: 502342323, // hopefully this isnt in use, TODO find free ports for testing
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		var count = 2,
+			decFn = function () {
+				count--;
+				if (count === 0)
+					done();
+			};
+
+		client.on("error", function (err) {
+			err.should.be.instanceof(Error);
+			decFn();
+		});
+
+		client.log("shouldn't work", function (err) {
+			err.should.be.instanceof(Error);
+			decFn();
+		});
+	});
+	it("should call on error on connection error Udp when invalid port", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("127.0.0.1", {
+			port: 12378726362, // hopefully this isnt in use, TODO find free ports for testing
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Udp
+		});
+
+		var count = 2,
+			decFn = function () {
+				count--;
+				if (count === 0)
+					done();
+			};
+
+		client.on("error", function (err) {
+			expect(err).to.be.instanceof(Error);
+			decFn();
+		});
+
+		client.log("shouldn't work", function (err) {
+			err.should.be.instanceof(Error);
+			decFn();
+		});
+	});
+	it("should call on error with timeout on connection error Tcp", function (done) {
+		var hostname = "testhostname";
+		var client = new syslogClient.createClient("203.0.113.1", {
+			port: syslogTcpPort, // hopefully this isnt in use, TODO find free ports for testing
+			tcpTimeout: 500,
+			syslogHostname: hostname,
+			transport: syslogClient.Transport.Tcp
+		});
+
+		var count = 2,
+			decFn = function () {
+				count--;
+				if (count === 0)
+					done();
+			};
+
+		client.on("error", function (err) {
+			err.should.be.instanceof(Error);
+			decFn();
+		});
+
+		client.log("shouldn't work", function (err) {
+			err.should.be.instanceof(Error);
+			decFn();
 		});
 	});
 });
