f46490812cbcd36a57fdb182f33b7659366d25e1
paulgrove <paul.a.grove@gmail.com>
2020-11-06 21:59:29 +0000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -7,19 +7,29 @@ export enum Transport {
 }
 
 export enum Facility {
-	Kernel =  0,
-	User   =  1,
-	System =  3,
-	Audit  = 13,
-	Alert  = 14,
-	Local0 = 16,
-	Local1 = 17,
-	Local2 = 18,
-	Local3 = 19,
-	Local4 = 20,
-	Local5 = 21,
-	Local6 = 22,
-	Local7 = 23
+	Kernel   =  0,
+	User     =  1,
+	Mail     =  2,
+	System   =  3,
+	Daemon   =  3,
+	Auth     =  4,
+	Syslog   =  5,
+	Lpr      =  6,
+	News     =  7,
+	Uucp     =  8,
+	Cron     =  9,
+	Authpriv = 10,
+	Ftp      = 11,
+	Audit    = 13,
+	Alert    = 14,
+	Local0   = 16,
+	Local1   = 17,
+	Local2   = 18,
+	Local3   = 19,
+	Local4   = 20,
+	Local5   = 21,
+	Local6   = 22,
+	Local7   = 23
 }
 
 export enum Severity {
